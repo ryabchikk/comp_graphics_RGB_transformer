60dba5328c8a57535b8216ef95d784ba8b5db7c9
ryabchikk <mixail-12-r@mail.ru>
2022-09-27 17:52:22 +0300

    add new form

---
+++ Form2.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Lab2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.PB_HIST_1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PB_HIST_2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PB_HIST_3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_HIST_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_HIST_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_HIST_3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(12, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 25);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "R";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PB_HIST_1
+            // 
+            this.PB_HIST_1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PB_HIST_1.Location = new System.Drawing.Point(12, 11);
+            this.PB_HIST_1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PB_HIST_1.Name = "PB_HIST_1";
+            this.PB_HIST_1.Size = new System.Drawing.Size(320, 320);
+            this.PB_HIST_1.TabIndex = 21;
+            this.PB_HIST_1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(338, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 25);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "G";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PB_HIST_2
+            // 
+            this.PB_HIST_2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PB_HIST_2.Location = new System.Drawing.Point(338, 11);
+            this.PB_HIST_2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PB_HIST_2.Name = "PB_HIST_2";
+            this.PB_HIST_2.Size = new System.Drawing.Size(320, 320);
+            this.PB_HIST_2.TabIndex = 23;
+            this.PB_HIST_2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(664, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 25);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "B";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PB_HIST_3
+            // 
+            this.PB_HIST_3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PB_HIST_3.Location = new System.Drawing.Point(664, 11);
+            this.PB_HIST_3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PB_HIST_3.Name = "PB_HIST_3";
+            this.PB_HIST_3.Size = new System.Drawing.Size(320, 320);
+            this.PB_HIST_3.TabIndex = 25;
+            this.PB_HIST_3.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 620);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.PB_HIST_3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PB_HIST_2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PB_HIST_1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.PB_HIST_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_HIST_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_HIST_3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        
+        public System.Windows.Forms.PictureBox PB_HIST_1;
+        public System.Windows.Forms.PictureBox PB_HIST_2;
+        public System.Windows.Forms.PictureBox PB_HIST_3;
+    }
+}
