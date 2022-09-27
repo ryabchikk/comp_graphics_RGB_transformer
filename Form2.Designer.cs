a83407e3ec02d2b4235d670714b303cfe07ee40b
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2022-09-28 01:07:31 +0300

    change button text

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -117,7 +117,7 @@ namespace Lab2
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(363, 678);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 36);
             this.button1.TabIndex = 27;
@@ -162,7 +162,7 @@ namespace Lab2
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 36);
             this.button2.TabIndex = 31;
-            this.button2.Text = "Draw Color Layers";
+            this.button2.Text = "Draw channels";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
