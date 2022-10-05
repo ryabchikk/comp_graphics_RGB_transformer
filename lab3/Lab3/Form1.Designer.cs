0cdf9dc0e4378ab83ff6f26ab44a69b34da725aa
Artrit0chka <58296372+Artrit0chka@users.noreply.github.com>
2022-10-05 14:56:15 +0300

    task 1c done

--- lab3/Lab3/Form1.Designer.cs
+++ lab3/Lab3/Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace Lab3
             this.button4 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox__t_1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,7 +45,7 @@ namespace Lab3
             // 
             this.button1.Location = new System.Drawing.Point(12, 61);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 32);
+            this.button1.Size = new System.Drawing.Size(124, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "Draw";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,7 +55,7 @@ namespace Lab3
             // 
             this.button2.Location = new System.Drawing.Point(12, 471);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 37);
+            this.button2.Size = new System.Drawing.Size(124, 37);
             this.button2.TabIndex = 1;
             this.button2.Text = "Task2";
             this.button2.UseVisualStyleBackColor = true;
@@ -64,7 +65,7 @@ namespace Lab3
             // 
             this.button3.Location = new System.Drawing.Point(12, 514);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(111, 32);
+            this.button3.Size = new System.Drawing.Size(124, 32);
             this.button3.TabIndex = 2;
             this.button3.Text = "Task3";
             this.button3.UseVisualStyleBackColor = true;
@@ -86,7 +87,7 @@ namespace Lab3
             // 
             this.Clear.Location = new System.Drawing.Point(12, 307);
             this.Clear.Name = "Clear";
-            this.Clear.Size = new System.Drawing.Size(111, 32);
+            this.Clear.Size = new System.Drawing.Size(124, 32);
             this.Clear.TabIndex = 4;
             this.Clear.Text = "Clear";
             this.Clear.UseVisualStyleBackColor = true;
@@ -96,7 +97,7 @@ namespace Lab3
             // 
             this.button4.Location = new System.Drawing.Point(12, 111);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(111, 32);
+            this.button4.Size = new System.Drawing.Size(124, 32);
             this.button4.TabIndex = 5;
             this.button4.Text = "Fill with color";
             this.button4.UseVisualStyleBackColor = true;
@@ -106,7 +107,7 @@ namespace Lab3
             // 
             this.button6.Location = new System.Drawing.Point(12, 345);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(111, 32);
+            this.button6.Size = new System.Drawing.Size(124, 32);
             this.button6.TabIndex = 6;
             this.button6.Text = "New Image";
             this.button6.UseVisualStyleBackColor = true;
@@ -116,17 +117,28 @@ namespace Lab3
             // 
             this.button5.Location = new System.Drawing.Point(12, 160);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(111, 32);
+            this.button5.Size = new System.Drawing.Size(124, 32);
             this.button5.TabIndex = 7;
             this.button5.Text = "Fill with texture";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 198);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(124, 32);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Fill with border";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(785, 582);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
@@ -153,6 +165,7 @@ namespace Lab3
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
     }
 }
 
