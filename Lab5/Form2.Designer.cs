341ee0f94784258b680d858869c2378c7de3e2a4
Artrit0chka <58296372+Artrit0chka@users.noreply.github.com>
2022-10-17 22:14:25 +0300

    Task 1 almost done

---
+++ Lab5/Form2.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Lab5
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.Create = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Iteration = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Iteration)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(42, 47);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(128, 45);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Open File";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(430, 47);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(141, 45);
+            this.Create.TabIndex = 1;
+            this.Create.Text = "Create";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Iterations:";
+            // 
+            // Iteration
+            // 
+            this.Iteration.Location = new System.Drawing.Point(283, 58);
+            this.Iteration.Name = "Iteration";
+            this.Iteration.Size = new System.Drawing.Size(62, 22);
+            this.Iteration.TabIndex = 3;
+            this.Iteration.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(929, 431);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 578);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Iteration);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.Open);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.Iteration)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Iteration;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
