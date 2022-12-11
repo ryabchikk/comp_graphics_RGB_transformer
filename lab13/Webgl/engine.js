deca0f50b0d59fabe52ad815e8de0ca77e45943c
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2022-12-11 15:47:19 +0300

    lab13 loading models and solar system

---
+++ lab13/Webgl/engine.js
@@ -0,0 +1,236 @@
+
+var vShaderSource = 
+`precision mediump float;
+    attribute vec3 vertPos;
+    attribute vec3 u_color;
+    attribute vec2 _textCoord;
+    varying vec3 color;
+    varying vec2 texCoord;
+    uniform mat4 view;
+    uniform mat4 projection;
+    uniform mat4 world;
+    void main()
+    {
+      color = u_color;
+      texCoord = _textCoord;
+      gl_Position = projection*view * world*vec4(vertPos,1.0);
+    }
+`
+
+var fShaderUniformSource = 
+`
+   precision mediump float;
+   varying vec3 color;
+   varying vec2 texCoord;
+   uniform float textureWeight;
+   uniform sampler2D Texture;
+    void main()
+    {
+      gl_FragColor = mix(vec4(color,1.0),texture2D(Texture,texCoord),textureWeight);
+    }
+`
+
+
+var VertexObjShader = 
+`   precision mediump float;
+    attribute vec3 vertPos;
+    attribute vec2 _textCoord;
+    varying vec2 texCoord;
+    uniform mat4 view;
+    uniform mat4 projection;
+    uniform mat4 world;
+  
+    void main()
+    { 
+     
+      texCoord = _textCoord;
+      gl_Position = projection*view * world*vec4(vertPos,1.0);
+    }
+`
+
+var FragmentObjShader = 
+`
+   precision mediump float;
+   varying vec2 texCoord;
+   uniform float textureWeight;
+   uniform sampler2D Texture;
+    void main()
+    {
+      gl_FragColor = texture2D(Texture,texCoord);
+    }
+`
+
+function GetTransformMatrix(x,y,z){
+  return new Float32Array([
+    1,0,0,0,
+    0,1,0,0,
+    0,0,1,0,
+    x,y,z,1
+ ]);
+}
+
+class Object {
+
+  constructor(scale_param,x,y,z) {
+    this.scale = scale_param;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+}
+
+
+var start = async function(){
+
+    var canvas = document.getElementById("test");
+    var gl = canvas.getContext('webgl');
+   
+
+    if(!gl){
+        console.log("error!");
+    }
+   
+    var tmp = await parseObjFile('knife_lowpoly.obj');
+    console.log(tmp);
+    SetDefaultSettings(gl);
+  
+    var vertexShader =  CreateShader(gl,gl.VERTEX_SHADER,VertexObjShader); 
+
+    var fragmentShader =  CreateShader(gl,gl.FRAGMENT_SHADER,FragmentObjShader);
+     
+   var cubeVertexPosBuffer = gl.createBuffer();
+   gl.bindBuffer(gl.ARRAY_BUFFER,cubeVertexPosBuffer);
+   gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(tmp),gl.DYNAMIC_DRAW);
+   
+   var texture = gl.createTexture();
+   gl.bindTexture(gl.TEXTURE_2D,texture);
+
+   gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.REPEAT);
+   gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.REPEAT);
+   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); 
+   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('crate'));
+
+ 
+    var program = CreateProgram(gl,vertexShader,fragmentShader);
+    gl.useProgram(program); 
+   
+     var pos_attr = gl.getAttribLocation(program,"vertPos");
+      
+     var text_attr = gl.getAttribLocation(program,"_textCoord");
+  
+     gl.enableVertexAttribArray(text_attr); 
+     var rotateAngle = MultiplayMatrix(GetRotationMatrixX(90),GetRotationMatrixY(40),4,4);
+
+     var transformData = await parseObjFile('cable_drum_low.obj');
+    
+     console.log(transformData);
+
+    var transformBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, transformBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(transformData), gl.DYNAMIC_DRAW);
+   
+    var main_texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D,main_texture);
+ 
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); 
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('main'));
+
+   
+     var viewmat4 = GetViewMatrix([0,0,-9],[0,0,0],[0,1,0]);
+     var projectionmat4 = GetPerspectiveMatrix(toRadians(45),canvas.width / canvas.clientHeight,0.1,900.0);
+     var view_matr = gl.getUniformLocation(program,'view');
+     var proj_matr = gl.getUniformLocation(program,'projection');
+     var world_matr = gl.getUniformLocation(program,'world');
+  
+     gl.uniformMatrix4fv(view_matr,gl.FALSE,viewmat4);
+     gl.uniformMatrix4fv(proj_matr,gl.FALSE,projectionmat4);
+    
+
+     let stepZ = 0;
+     count = 3;
+   
+   stepY = 0;
+   stepZ = 0;
+
+   const scale_param = 0.06; // *вынести в класс
+   const rotation_speed = 20; // *вынести в класс
+   rotateAngle = MultiplayMatrix(GetRotationMatrixZ(90),GetRotationMatrixY(90),4,4);
+   var rotateAngle_main = MultiplayMatrix(GetRotationMatrixX(40),GetRotationMatrixY(40),4,4);
+   stepY = 0;
+   stepZ = 0;
+   const scale = [0.06,0.1,0.01,0.03,0.06]; // *вынести в класс (параметров scale double)
+   var rotation = 0;
+   
+   // *внести изменения после рефакторинга
+   const frame = () => {
+
+     //MUST HAVE прикрутить камеру 
+    /* 
+    для чего:
+    1)подвязать кнопки
+    2)Прокинуть аргументы arg - массив arg[0] =x , arg[1] = y , arg[2] = z в видовую матрицу 
+      
+    Видовая матрица:
+      var viewmat4 = GetViewMatrix([arg[0],arg[1],arg[2]],[0,0,0],[0,1,0]);
+
+    3)Передать новую видовую матрицу в gpu 
+     gl.uniformMatrix4fv(view_matr,gl.FALSE,viewmat4);
+    
+    */
+
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+     
+    gl.disableVertexAttribArray(pos_attr);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, transformBuffer);
+    gl.vertexAttribPointer(pos_attr,3,gl.FLOAT,gl.FALSE,8*Float32Array.BYTES_PER_ELEMENT,0);
+    gl.enableVertexAttribArray(pos_attr);
+    rotateAngle_main =  MultiplayMatrix(GetRotationMatrixY(rotation_speed*Math.PI/180),rotateAngle_main,4,4);
+   var main_m4 = MultiplayMatrix(GetScaleMatrix(scale_param/2,scale_param/2,scale_param/2),rotateAngle_main,4,4);
+  
+    gl.bindTexture(gl.TEXTURE_2D,main_texture);
+       
+    gl.activeTexture(gl.TEXTURE0);
+    gl.vertexAttribPointer(text_attr,2,gl.FLOAT,gl.FALSE,8*Float32Array.BYTES_PER_ELEMENT,3*Float32Array.BYTES_PER_ELEMENT);
+    gl.uniformMatrix4fv(world_matr,gl.FALSE,main_m4);
+    for(let i = 0;i<transformData.length;i+=3){
+      gl.drawArrays(gl.TRIANGLES, i,3);
+      } 
+  
+      //рисуем второстепенные объекты
+      gl.disableVertexAttribArray(pos_attr);
+    gl.bindBuffer(gl.ARRAY_BUFFER,cubeVertexPosBuffer);
+    gl.vertexAttribPointer(pos_attr,3,gl.FLOAT,gl.FALSE,8*Float32Array.BYTES_PER_ELEMENT,0);
+    gl.enableVertexAttribArray(pos_attr);
+    gl.bindTexture(gl.TEXTURE_2D,texture);   
+    gl.activeTexture(gl.TEXTURE0);
+    gl.vertexAttribPointer(text_attr,2,gl.FLOAT,gl.FALSE,8*Float32Array.BYTES_PER_ELEMENT,3*Float32Array.BYTES_PER_ELEMENT);
+
+    for(var i = -5;i<=5;i+=2.5){
+     rotateAngle = MultiplayMatrix(GetRotationMatrixX(rotation_speed*Math.PI/180),rotateAngle,4,4);
+    // stepZ += rotation_speed*Math.PI/180;
+      var x = 2*Math.cos(i+0.1*rotation*Math.PI/(180));
+      var z = 2*Math.sin(i+0.1*rotation*Math.PI/(180));
+      var  worldm4 = GetTransformMatrix(x,0,z);
+     
+      worldm4 = MultiplayMatrix(rotateAngle,worldm4,4,4);
+      worldm4 = MultiplayMatrix(GetScaleMatrix(scale[rotation % 5],scale[rotation % 5],scale[rotation % 5]),worldm4,4,4);
+      gl.uniformMatrix4fv(world_matr,gl.FALSE,worldm4);
+      rotation++;
+      for(let i = 0;i<tmp.length;i+=3){
+       gl.drawArrays(gl.TRIANGLES, i,3);
+       } 
+    }    
+          requestAnimationFrame(frame);
+    }   
+   frame();
+
+        
+};
---
+++ lab13/Webgl/help.js
@@ -0,0 +1,331 @@
+function toRadians (angle) {
+    return angle * (Math.PI / 180);
+  }
+  
+  function GetIdentity(){
+     return new Float32Array([
+        1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,0,1
+     ]);
+  }
+  
+  //только вектор из 3 элементов
+  function Normalize(v,value){
+     if(value == 0)
+         return [0,0,0];
+  
+     return [v[0]*value,v[1]*value,v[2]*value];
+  }
+  
+  function CrossProduct(v1,v2,value,normalize){
+     var res = [
+        v1[1]*v2[2] - v1[2]*v2[1],
+        v1[2]*v2[0] - v1[0]*v2[2],
+        v1[0]*v2[1] - v1[1]*v2[0],
+     ]
+     if(normalize){
+       Normalize(res,value);
+     }
+     return res;
+  }
+  
+  //по сути реализует статичную камеру
+  // да-да не учитываем,что viewer может совпадать с LookingPoint
+  function GetViewMatrix(viewer,lookingPoint,pointingUp){
+  
+     //считаем расстояние от нас
+    var z0 = viewer[0] - lookingPoint[0];
+    var z1 = viewer[1] - lookingPoint[1];
+    var z2 = viewer[2] - lookingPoint[2];
+     
+    var len = Math.hypot(z0,z1,z2); // нормализуем
+     z0 *= 1/len;
+     z1 *= 1/len;
+     z2 *= 1/len;
+    
+   var x = CrossProduct(pointingUp,[z0,z1,z2],1/len,true);
+   var y = CrossProduct([z0,z1,z2],x,0,false);
+   len =  Math.hypot(y[0], y[1], y[2]);
+   var y = Normalize(y,len);
+    
+   return new Float32Array([ x[0], y[0], z0 ,0, 
+                             x[1], y[1] , z1, 0,
+                             x[2], y[2], z2, 0,
+                             -(x[0]*viewer[0]+x[1]*viewer[1]+x[2]*viewer[2]),
+                             -(y[0]*viewer[0]+y[1]*viewer[1]+y[2]*viewer[2]), 
+                             -(z0*viewer[0]+z1*viewer[1]+z2*viewer[2]),
+                              1
+                          ]);
+  }
+  
+ 
+  
+  function GetRotationMatrixX(angle){
+     angle = toRadians(angle);
+     var cos = Math.cos(angle);
+     var sin = Math.sin(angle);
+     return new Float32Array([
+        1,0,0,0,
+        0,cos,sin,0,
+        0,-sin,cos,0,
+        0,0,0,1
+     ]);
+  }
+  
+  function GetRotationMatrixY(angle){
+     angle = toRadians(angle);
+     var cos = Math.cos(angle);
+     var sin = Math.sin(angle);
+     return new Float32Array([
+        cos,0,-sin,0,
+        0,1,0,0,
+        sin,0,cos,0,
+        0,0,0,1
+     ]);
+  }
+  
+  
+function GetScaleMatrix(x,y,z){
+   return new Float32Array([
+      x,0,0,0,
+      0,y,0,0,
+      0,0,z,0,
+      0,0,0,1
+   ]);
+}
+
+  function GetRotationMatrixZ(angle){
+     angle = toRadians(angle);
+     var cos = Math.cos(angle);
+     var sin = Math.sin(angle);
+     return new Float32Array([
+        cos,sin,0,0,
+        -sin,cos,0,0,
+        0,0,1,0,
+        0,0,0,1
+     ]);
+  }
+  
+  function MultiplayMatrix(m1,m2,row,col){
+     var res = new Float32Array(row*col);
+     for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            let sum = 0;
+            for (let k = 0; k < col; k++)
+                sum += m1[i * col + k] * m2[k * col + j];
+            res[i * col + j] = sum;
+        }
+    }
+    return res;
+  }
+  
+  
+  function GetPerspectiveMatrix(fov,aspect,nearDist,farDist){
+  
+     var res = new Float32Array(16);
+     var uh = 1.0/Math.tan(fov/2);
+     res[0] = uh / aspect;
+     res[1] = 0;res[2] = 0;res[3] = 0;res[4] = 0;
+     res[5] = uh;res[6] = 0;res[7] = 0;res[8] = 0;
+     res[9] = 0;res[11] = -1;res[12] = 0;res[13] = 0;
+     res[15] = 0;
+  
+     var fd = 1/(nearDist - farDist);
+     res[10] = (farDist+nearDist)*fd;
+     res[14] = 2*farDist*nearDist*fd;
+     return res;
+  }
+  
+ 
+ function CreateShader(gl,type,shaderSource){
+   var shader = gl.createShader(type);
+   gl.shaderSource(shader,shaderSource);
+ 
+   gl.compileShader(shader);
+   
+   if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)){
+    console.log('ERROR compiling shader');
+    console.log(gl.getShaderInfoLog(shader));
+ } 
+ 
+  return shader;
+ 
+ }
+ 
+ 
+ function CreateProgram(gl,vertexShader,fragmentShader){
+    var program = gl.createProgram();
+    gl.attachShader(program,vertexShader);
+    gl.attachShader(program,fragmentShader);
+    gl.linkProgram(program);
+ 
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+       console.log('ERROR compiling program');
+       console.log(gl.getProgramInfoLog(program));
+    }
+ 
+    gl.validateProgram(program);
+     if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+         console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+         return null;}
+ 
+    return program;
+ }
+ 
+ 
+ 
+ function GetCubeCoordinates(){
+   return new Float32Array([
+    //top            color           texture
+    -1.0,1.0,-1.0,   1.0,0.0,0.0, 0.0,0.0,
+    -1.0,1.0,1.0,   0.0,1.0,0.0,  0.0,1.0, 
+    1.0,1.0,1.0,   0.0,0.0,1.0,   1.0,1.0,
+    1.0,1.0,-1.0,   1.0,1.0,0.0,  1.0,0.0,
+    
+    //left
+    -1.0,1.0,1.0,   1.0,0.0,0.0,   0.0,0.0,
+    -1.0,-1.0,1.0,   0.0,1.0,0.0,  1.0,0.0,
+    -1.0,-1.0,-1.0,   0.0,0.0,1.0, 1.0,1.0,
+    -1.0,1.0,-1.0,   1.0,1.0,0.0,  0.0,1.0,
+ 
+    //right
+    1.0,1.0,1.0,  1.0,0.0,0.0,    1.0,1.0,
+    1.0,-1.0,1.0,   0.0,1.0,0.0,   0.0,1.0,
+    1.0,-1.0,-1.0,   0.0,0.0,1.0,  0.0,0.0,
+    1.0,1.0,-1.0,   1.0,1.0,0.0,   1.0,0.0,
+    
+    //front
+    1.0,1.0,1.0,   1.0,0.0,0.0,   1.0,1.0,
+    1.0,-1.0,1.0,   0.0,1.0,0.0,  1.0,0.0,
+    -1.0,-1.0,1.0,   0.0,0.0,1.0, 0.0,0.0,
+    -1.0,1.0,1.0,   1.0,1.0,0.0,  0.0,1.0,
+    
+    //back
+    1.0,1.0,-1.0,   1.0,0.0,0.0, 0.0, 0.0,
+    1.0,-1.0,-1.0,   0.0,1.0,0.0, 0.0, 1.0,
+    -1.0,-1.0,-1.0,   0.0,0.0,1.0, 1.0, 1.0,
+    -1.0,1.0,-1.0,   1.0,1.0,0.0,  1.0, 0.0,
+ 
+    //bottom
+    -1.0,-1.0,-1.0,   1.0,0.0,0.0, 1.0, 1.0,
+    -1.0,-1.0,1.0,   0.0,1.0,0.0,  1.0, 0.0,
+    1.0,-1.0,1.0,   0.0,0.0,1.0,   0.0, 0.0,
+    1.0,-1.0,-1.0,   1.0,1.0,0.0,  0.0, 1.0
+   ]);
+ }
+ 
+ function GetCubeIndeces(){
+    return new Uint16Array([
+       //top
+       0,1,2,
+       0,2,3,
+ 
+       //left
+       5,4,6,
+       6,4,7,
+ 
+       //right
+       8,9,10,
+       8,10,11,
+ 
+       //front
+       13,12,14,
+       15,14,12,
+ 
+       //back
+       16,17,18,
+       16,18,19,
+       
+       //bottom
+       21,20,22,
+       22,20,23
+    ]);
+ }
+ 
+ 
+ function SetDefaultSettings(gl){
+    gl.clearColor(0.7,0.7,0.7,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+ }
+ 
+ function DrawObject(gl,program,viewmat4,projectionmat4,worldm4,coords){
+    
+   var view_matr = gl.getUniformLocation(program,'view');
+   var proj_matr = gl.getUniformLocation(program,'projection');
+   var world_matr = gl.getUniformLocation(program,'world');
+
+     gl.uniformMatrix4fv(world_matr,gl.FALSE,worldm4);
+     gl.uniformMatrix4fv(view_matr,gl.FALSE,viewmat4);
+     gl.uniformMatrix4fv(proj_matr,gl.FALSE,projectionmat4);
+     
+     for(let i = 0;i<coords.length;i+=3){
+      gl.drawArrays(gl.TRIANGLES, i,3);
+      } 
+}
+
+
+async function parseObjFile(path){
+   const response = await fetch(path);
+   const text = await response.text();
+   
+   const vertexObj =  [];
+   const textureObj = [];
+   const normalObj = [];
+   var webglCoords = [];
+
+   const ComplexData = [
+      vertexObj,
+      textureObj,
+      normalObj
+   ]
+
+    
+   text.split('\n').forEach(element => {
+     const elements = element.split(/\s+/);
+      if(elements[0] === "v"){
+         //console.log('enter_v');
+         vertexObj.push(elements.slice(1).map(parseFloat));
+      } 
+      else if(elements[0] === "vt"){
+         //console.log('enter_vt');
+         textureObj.push(elements.slice(1).map(parseFloat));
+      }
+      else if(elements[0] === "vn"){
+         //console.log('enter_vn');
+         normalObj.push(elements.slice(1).map(parseFloat));
+      }
+      else if(elements[0] === "f"){
+        // f 1/1/1 5/2/1 7/3/1 3/4/1 
+       //console.log('enter');
+        function AddByIndex(position){
+         var pos =position.split('/');
+         if(pos[0] === '') 
+          return;
+         pos.forEach((ind,i) =>{
+            if(normalObj.length === 0) 
+             return;
+            var parsedInd = parseInt(ind);
+            const Ind = (parsedInd >= 1 ? parsedInd - 1 : parsedInd + ComplexData[i].length);
+            if(ComplexData[i][Ind] === undefined)
+              console.log(i,Ind,pos);
+            webglCoords.push(...ComplexData[i][Ind]);
+         }
+         );           
+        }
+
+         const positions = elements.slice(1);
+         for(let i = 0;i<positions.length-2;i++){
+             AddByIndex(positions[0]);
+             AddByIndex(positions[i+1]);
+             AddByIndex(positions[i+2]);
+         }
+      }
+     
+   });
+
+   return webglCoords;      
+}
+
